b324a107ba77e698b246c1462b82e388b855d0a9
Pranjal9399 <pranjaltiwari0104@gmail.com>
2026-01-01 21:11:09 +0530

    six commit

--- jan_1.py
+++ jan_1.py
@@ -1,6 +1,8 @@
 a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 c=int(input("Enter third number:"))
+d=int(input("Enter fourth number:"))
+e=int(input("Enter fifth number:"))
 
 
-print (a-b*c)
+print (a+b+c-d*e)
