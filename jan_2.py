a614082678092367776b68e5a766b82b91852504
Pranjal9399 <pranjaltiwari0104@gmail.com>
2026-01-04 15:48:39 +0530

    Add NumPy practice files (jan_1.py, jan_2.py)

---
+++ jan_2.py
@@ -0,0 +1,17 @@
+import numpy as np 
+
+zeroes=np.zeros((3,4))
+print(zeroes)
+
+ones=np.ones((3,4))
+print(ones)
+
+full=np.full((3,4),7)
+print(full)
+
+random=np.random.random((3,4))
+print(random)
+
+sequence=np.arange(0,20,2)
+print(sequence)
+
